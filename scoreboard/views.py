ab33224c1998fbaa240591e8a7324348716619ff
Monte Hellawell <monte@montudor.com>
2020-12-09 01:42:06 +0000

    Prevent multiple scoreboard entries

--- scoreboard/views.py
+++ scoreboard/views.py
@@ -1,7 +1,40 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from django.utils import timezone
 from .serializers import ScoreSerializer
 from .models import Score
 
 class ScoreViewSet(viewsets.ModelViewSet):
     queryset = Score.objects.all().order_by('-score', 'level')
     serializer_class = ScoreSerializer
+
+    def create(self, request):
+        serializer = ScoreSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response({'detail': 'Invalid score object'}, status=400)
+
+
+        scores = Score.objects.filter(name=serializer.data['name']).order_by('-score', 'level')
+        score = None
+
+        if scores.count() > 0:
+            score = scores[0]
+            if score.score < serializer.data['score']:
+                score.score = serializer.data['score']
+                score.level = serializer.data['level']
+                score.created = timezone.now()
+                score.save()
+            serializer = ScoreSerializer(score)
+            return Response(serializer.data)
+
+        score = Score(
+            name=serializer.data['name'],
+            score=serializer.data['score'],
+            level=serializer.data['level']
+        )
+        score.save()
+
+        serializer = ScoreSerializer(score)
+
+        return Response(serializer.data)
