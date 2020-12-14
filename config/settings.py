1dc109bb7a4dccf9e0a7fdd93354cd8993e23b7e
Monte Hellawell <monte@montudor.com>
2020-12-14 18:18:56 +0000

    Update how secret key is sourced

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+bqwv&*h)cu!b0p211pxzesj%jvuws8c*i78ayk@su8vl0n8pj'
+SECRET_KEY = os.environ.get('SECRET_KEY', '+bqwv&*h)cu!b0p211pxzesj%jvuws8c*i78ayk@su8vl0n8pj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
